ff4f587046e4f47c239559daa60cb85382718abc
PureLearning-dev <liu.jie.coder@gmail.com>
2026-09-16 22:15:48 +0800

    chore: initialize PmHub project

---
+++ src/main/java/com/purelearning/pmhub/PmHubApplication.java
@@ -0,0 +1,13 @@
+package com.purelearning.pmhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PmHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PmHubApplication.class, args);
+    }
+
+}
